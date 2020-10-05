aafa0b947ec9aabc89a3f555192008723bcb34ce
premchandAllu <premchandallu1999@gmail.com>
2020-10-05 11:10:08 +0530

    Added: Board is Created

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,24 @@
 package com.capgemini.TicTacToe;
+import java.util.*;
 
 public class TicTacToe {
 
+	// Create board with Empty Spaces
+	private char[] createBoard() {
+		char[] board = new char[10];
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Tic Tac Toe game");
+		// Welcome message
+		System.out.println("Welcome to TicTacToe program");
+
+		TicTacToe cb = new TicTacToe();
+		cb.createBoard();
 	}
 
 }
