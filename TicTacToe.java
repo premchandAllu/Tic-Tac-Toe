bbded31806e9aa7ff50e5f9ddc64418fab2f0501
premchandAllu <premchandallu1999@gmail.com>
2020-10-05 11:14:56 +0530

    Added: User input is taken

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,22 +3,38 @@ import java.util.*;
 
 public class TicTacToe {
 
-	// Create board with Empty Spaces
-	private char[] createBoard() {
+	// Variables
+	private static char computer;
+	private static char[] board;
+	private static char userChoice;
+
+	// Create Board and Assign Empty Spaces
+	private static char[] createBoard() {
 		char[] board = new char[10];
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < board.length; i++)
 			board[i] = ' ';
-		}
 		return board;
 	}
 
-	public static void main(String[] args) {
-
-		// Welcome message
-		System.out.println("Welcome to TicTacToe program");
-
-		TicTacToe cb = new TicTacToe();
-		cb.createBoard();
+	// Selection of Letter by User
+	private static char chooseUserLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose Your User Letter X or O");
+		char userChoice = sc.next().charAt(0);
+		sc.close();
+		return userChoice;
 	}
 
+	public static void main(String[] args) {
+	
+		board = createBoard();
+		userChoice = chooseUserLetter();
+
+		// Assigning Letter to Computer
+		if (userChoice == 'X')
+			computer = 'O';
+		else
+			computer = 'X';
+		System.out.println("Computer letter is " + computer);
+	}
 }
